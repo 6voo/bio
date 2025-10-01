8ad01c1d6190b386b5c0607bc26949669309885b
6voo <juliaoncrack@gmail.com>
2025-10-01 13:54:56 -0400

    Styling bio, added profile picture and bio details

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,10 @@
 var landing = document.getElementById("landing");
+var bio = document.getElementById("bio");
 var audio = document.getElementById("audio");
 
 function playMedia() {
     landing.style.opacity = 0;
+    bio.style.opacity = 100;
     landing.style.pointerEvents = 'none';
     audio.play()
 }
